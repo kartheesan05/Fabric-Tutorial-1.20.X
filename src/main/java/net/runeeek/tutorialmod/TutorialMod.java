152a799fb1e3eeb198e770cf50a40e0db652e0ea
Kartheesan69 <2022cs0138@svce.ac.in>
2024-05-31 00:06:36 +0530

    test

--- src/main/java/net/runeeek/tutorialmod/TutorialMod.java
+++ src/main/java/net/runeeek/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.runeeek.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.runeeek.tutorialmod.item.ModItemGroups;
+import net.runeeek.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
